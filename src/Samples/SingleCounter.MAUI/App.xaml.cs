643770b7327a2d6e5a84d87e8e40dfd24ad03b8f
Joshua Marner <biz@jmarner.com>
2023-08-22 10:48:05 -0500

    Add a maui sample project

---
+++ src/Samples/SingleCounter.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SingleCounter.MAUI
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
